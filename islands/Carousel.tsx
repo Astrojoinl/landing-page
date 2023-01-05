04f0d9a2e581ac1ee186c61cc87ae448fb4b71b5
Edward <esoto@e-learning.com.co>
2023-01-05 11:09:59 -0500

    Carousel better styling

--- islands/Carousel.tsx
+++ islands/Carousel.tsx
@@ -7,26 +7,26 @@ export default function Carousel({cardsize, content}){
   const MAX_VISIBILITY = 3
 
   return (
-    <div class="carousel">
+    <div>
       {active > 0 && <button class='nav left' onClick={() => setActive(i => i - 1)}><img src="/left_arrow_next.svg"/></button>}
-
-      {cards.map((child, i)=> (
-        <div class="card-container" style={{
-          '--active': i === active ? 1 : 0,
-          '--offset': (active - i) / 3,
-          '--direction': Math.sign(active - i),
-          '--abs-offset': Math.abs(active - i) / 3,
-          'pointer-events': active === i ? 'auto' : 'none',
-          'opacity': Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1',
-          'display': Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block',
-        }}>
-          <div class="card">
-            <h2>{`Card ${i}`}</h2>
-            <p>{child}</p>
+      <div class="carousel">
+        {cards.map((child, i)=> (
+          <div class="card-container" style={{
+            '--active': i === active ? 1 : 0,
+            '--offset': (active - i) / 3,
+            '--direction': Math.sign(active - i),
+            '--abs-offset': Math.abs(active - i) / 3,
+            'pointer-events': active === i ? 'auto' : 'none',
+            'opacity': Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1',
+            'display': Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block',
+          }}>
+            <div class="card">
+              <h2>{`Card ${i}`}</h2>
+              <p>{child}</p>
+            </div>
           </div>
-        </div>
-      ))}
-
+        ))}
+      </div>
       {active < cardsize - 1 && <button class='nav right' onClick={() => setActive(i => i + 1)}><img src="/right_arrow_next.svg"/></button>}
     </div>
   )
