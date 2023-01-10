f1eb4177ec2ebf967e2ea731e3f8a8a1fdb40c4c
Edward <esoto@e-learning.com.co>
2023-01-10 13:19:29 -0500

    Navigation bar & Carousel integration

--- islands/Carousel.tsx
+++ islands/Carousel.tsx
@@ -4,35 +4,29 @@ const MAX_VISIBILITY = 3
 export default function Carousel({content}){
   const cards_size = content.length
   const [active, setActive] = useState(0)
-  setInterval(() => {
-    if(active >= cards_size - 1) setActive(0)
-    else setActive(active + 1)
-  }, 10000 )
   return (
       <div class="carousel">
-      <button class='nav left' onClick={() => setActive(i => {
-        if(i <= 0) return cards_size - 1
-        else return i - 1
-      })}><img src="/Icons/left arrow.svg"/></button>
-      {content.map((child, i)=> (
-        <div class="card-container" style={{
-          '--active': i === active ? 1 : 0,
-          '--offset': (active - i) / 3,
-          '--direction': Math.sign(active - i),
-          '--abs-offset': Math.abs(active - i) / 3,
-          'pointer-events': active === i ? 'auto' : 'none',
-          'opacity': Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1',
-          'display': Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block',
-        }}>
-          <div class="card" style={{overflow: "hidden"}}>
-            <video class="card-content" src={`/Carousel/${child}`} autoplay="true" loop="true" muted="true"/>
+        <button class='navi left' onClick={() => setActive(active <= 0 ? cards_size - 1 : active - 1)}><img src="/Icons/left arrow.svg"/></button>
+        {content.map((child, i)=> (
+          <div class="card-container" style={{
+            '--active': i === active ? 1 : 0,
+            '--offset': (active - i) / 3,
+            '--direction': Math.sign(active - i),
+            '--abs-offset': Math.abs(active - i) / 3,
+            'pointer-events': active === i ? 'auto' : 'none',
+            'opacity': Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1',
+            'display': Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block',
+          }}>
+            <div class="card">
+              <video class="card-content" src={`/Carousel/${child}`} autoplay="true" loop="true" muted="true"/>
+            </div>
           </div>
-        </div>
-      ))}
-      <button class='nav right' onClick={() => setActive(i => {
-        if(i >= cards_size - 1) return 0
-        else return i + 1
-      })}><img src="/Icons/left arrow.svg" style={{transform: "scaleX(-1)"}}/></button>
+        ))}
+        <button class='navi right' onClick={() => setActive(active >= cards_size - 1 ? 0 : active + 1)}><img src="/Icons/left arrow.svg" style={{transform: "scaleX(-1)"}}/></button>
+        {/* {setInterval(() => {
+          if(active >= cards_size - 1) setActive(0)
+          else setActive(active + 1)
+        }, 10000 )} */}
       </div>
   )
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -17,6 +17,29 @@ export default function Index({data}) {
       <Head>
         <link rel="stylesheet" href="/styles.css" />
       </Head>
+
+      <nav class="nav">
+          <div class="p-container">
+              <div class="logo">
+                  <a href="#">Your Logo</a>
+              </div>
+              <div id="mainListDiv" class="main_list">
+                  <ul class="navlinks">
+                      <li><a href="#">About</a></li>
+                      <li><a href="#">Portfolio</a></li>
+                      <li><a href="#">Services</a></li>
+                      <li><a href="#">Contact</a></li>
+                  </ul>
+              </div>
+              <span class="navTrigger">
+                  <i></i>
+                  <i></i>
+                  <i></i>
+              </span>
+          </div>
+      </nav>
+
+
       <Carousel content={data} />
     </Fragment>
   )
