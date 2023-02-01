b99f2db681f089ec116174ebffcfaab194ba31c9
Edward Soto <esoto@e-learning.com.co>
2023-02-01 14:48:42 -0500

    responsive feature finish

--- islands/NavBar.jsx
+++ islands/NavBar.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "preact/hooks"
 
 export default function NavBar() {
   const [scrolled, setScrolled] = useState(false)
+  const [clicked_menu, setClickedMenu] = useState(false)
   useEffect(() => {
     // deno-lint-ignore no-window-prefix
     window.addEventListener('scroll', () => {
@@ -16,20 +17,16 @@ export default function NavBar() {
       <Head>
         <link rel="stylesheet" href="styles/NavBar.css" />
       </Head>
-
-
       <nav class={scrolled ? "nav affix" : "nav no-affix"}>
         <div class="logo">
           <a href="#"><img src="logos/logo.gif"/></a>
         </div>
-
-        <span class="navTrigger active">
+        <span class={clicked_menu ? "navTrigger active" : "navTrigger"} onClick={() => setClickedMenu(!clicked_menu)}>
           <i></i>
           <i></i>
           <i></i>
         </span>
-
-        <div id="mainListDiv" class="main_list show_list">
+        <div id="mainListDiv" class={clicked_menu ? "main_list show_list" : "main_list"}>
           <ul class="navlinks">
             <li><a href="#facts"><b>Cifras</b></a></li>
             <li><a href="#benefits"><b>Servicios</b></a></li>
@@ -37,7 +34,6 @@ export default function NavBar() {
             <li><a href="#registration" id="contact"><b>Contacto</b></a></li>
           </ul>
         </div>
-
       </nav>    
     </Fragment>
   )
